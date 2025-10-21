bb2e93fdd8b31f2f44e3413bd3720921c4fbb329
sashaprykhodko <CaH4aZzz@gmail.com>
2025-10-21 22:24:44 +0300

    Basic spring initialization

---
+++ src/test/java/com/oprykhodko/recipeplatformbackend/RecipeplatformbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oprykhodko.recipeplatformbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeplatformbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
