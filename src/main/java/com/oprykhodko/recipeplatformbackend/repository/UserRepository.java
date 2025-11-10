3feabd496f972ca71092841b7378d463d1ffb91d
sashaprykhodko <CaH4aZzz@gmail.com>
2025-11-10 23:53:07 +0200

    added basic UserRepository

---
+++ src/main/java/com/oprykhodko/recipeplatformbackend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.oprykhodko.recipeplatformbackend.repository;
+
+import com.oprykhodko.recipeplatformbackend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
